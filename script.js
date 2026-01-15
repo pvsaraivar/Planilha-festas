29c54698ce2783f2c07bb525c9619a8451ff98ba
Paulo Victor Saraiva <pvsaraivs@hotmail.com>
2026-01-15 13:33:00 -0300

    .

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ const eventImageMap = {
     'pacific de janeiro': 'assets/pacificdejaneiro.PNG',
     'dabysha': 'assets/dabysha.PNG',
     'longdreams showcase': 'assets/longdreamsshowcase.PNG',
-    'loren party - all black': 'assets/lorendayallblack.PNG'],
+    'loren party - all black': 'assets/lorendayallblack.PNG',
     'plano aberto - germinação': 'assets/planoabertogerminasao.PNG',
     'house music culture 3': 'assets/hmc3.PNG',
     'oscvra - impuria': 'assets/oscvraimpuria.PNG',
