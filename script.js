bdce9d3f21ca1865046e0c303c16bc677d184c45
Paulo Victor Saraiva <pvsaraivs@hotmail.com>
2026-01-21 17:24:59 -0300

    .

--- script.js
+++ script.js
@@ -156,7 +156,10 @@ const eventImageMap = {
     'baile freak 2026': 'assets/blocofreak.mp4',
     'trance on board': 'assets/tranceonboard.PNG',
     'hibrida': 'assets/hibrida.mp4',
-    'melange no arrumação': 'assets/melange2.PNG'
+    'melange no arrumação': 'assets/melange2.PNG',
+    'kaliente': 'assets/kaliente.PNG',
+    'kaza sessions - ebony e duquesa': 'assets/kazasessionsnandi.PNG',
+    'splash party': 'assets/splashparty.PNG'
 }
 
 /**
