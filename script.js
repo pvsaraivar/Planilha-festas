10fb8b68c320ff3ac9501670ea065b857e598181
Paulo Victor Saraiva <pvsaraivs@hotmail.com>
2026-01-30 12:15:20 -0300

    .

--- script.js
+++ script.js
@@ -178,7 +178,8 @@ const eventImageMap = {
     'pre carnaval fortaleza - 01/02 - raimundo dos queijos': 'assets/prefortaleza01022.PNG',
     'calúnia - delírio tropical': 'assets/calunia3.PNG',
     'jackwav bday': 'assets/jackwav.PNG',
-    'festa base': 'assets/clubedaprancha2.PNG'
+    'festa base': 'assets/clubedaprancha2.PNG',
+    'bateu long set - lucas bmr': 'assets/bmrlongset.PNG'
 }
 
 /**
