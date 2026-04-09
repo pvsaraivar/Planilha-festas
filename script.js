8c4b12582cdb542c22f637874eccd9f44132e7ec
Paulo Victor Saraiva <pvsaraivs@hotmail.com>
2026-04-09 19:15:24 -0300

    .

--- script.js
+++ script.js
@@ -314,7 +314,7 @@ const eventImageMap = {
     'summer dreams': 'assets/summerdreams.jpg',
     'ovo frito 17': 'assets/ovofrito17.jpg',
     'link.a': 'assets/link2.jpg',
-    'fritaria sangue latino': 'assets/fritariasanguelatino.mp4',
+    'fritaria sangue latino': 'assets/fritariasl.jpg',
     'beije #2': 'assets/beije2.jpg',
     'gasguita elétrica': 'assets/gasgeletrica.jpg',
     'naturaiz ce': 'assets/naturaiz.jpg',
