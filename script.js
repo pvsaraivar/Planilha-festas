8620b7aa7c934ca1796f82a260a494ce09852893
Paulo Victor Saraiva <pvsaraivs@hotmail.com>
2026-06-30 18:40:33 -0300

    .

--- script.js
+++ script.js
@@ -386,7 +386,9 @@ const eventImageMap = {
     'warm up fortal': 'assets/warmupfortal.jpg',
     'after da guapo': 'assets/afterguapo.jpg',
     'gostoso: eden': 'assets/gostosoeden.jpg',
-    'wav sessions de férias': 'assets/wavferias.jpg'
+    'wav sessions de férias': 'assets/wavferias.jpg',
+    'housefi': 'assets/housefi.jpg',
+    'sado xtreme': 'assets/sadoxtreme.jpg'
 }
 
 /**
